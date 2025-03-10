f8bfada2692199811aab860408fc7c929ac7fa44
1cbyc <isaacnsisong@gmail.com>
2025-03-11 02:55:13 +0700

    updating the backend

--- frontend/src/components/GreeksVisualization.jsx
+++ frontend/src/components/GreeksVisualization.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Box, Paper, Typography, Grid, Tabs, Tab } from '@mui/material';
 import * as d3 from 'd3';
 import { styled } from '@mui/material/styles';
-import init, { calculate_option_price } from '../wasm/pkg/options_pricing';
+import init, { calculate_option_price } from '/wasm/options_pricing';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(3),
